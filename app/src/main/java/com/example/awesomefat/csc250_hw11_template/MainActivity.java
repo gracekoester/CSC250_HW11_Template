2783ba029ec353dcc81a7da0b4108c36a394f09f
Grace <gmk1914@yahoo.com>
2017-11-06 21:42:17 -0600

    added comment

--- app/src/main/java/com/example/awesomefat/csc250_hw11_template/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/csc250_hw11_template/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        //Grace Koester
+        /* Grace */
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
     }
